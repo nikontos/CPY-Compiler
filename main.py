c5b11d318f1206f60679e34a05805c5df5ad7eac
Nikolaos-Marios Kontos <katsikiwths@gmail.com>
2023-03-03 18:20:14 +0200

    Small changse at main for better testing
    While loop added that terminates at EOF

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ if __name__ == '__main__':
     test = lex.Lex('test.cpy')
     b = test.next_token()
 
-    print("Token: " + b.recognised_string + " Line: " + str(b.line_number) + " Family: " + b.family)
-    b = test.next_token()
-    print("Token: " + b.recognised_string + " Line: " + str(b.line_number) + " Family: " + b.family)
+    while b != "EOF":
+        print("Token: " + b.recognised_string + " Line: " + str(b.line_number) + " Family: " + b.family + " Pos:" + str(
+            test.position))
+        print("The int number is :: " + str(int(b.recognised_string) + 420))
+        b = test.next_token()
+
+
 
